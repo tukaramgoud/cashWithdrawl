6dd20a8af26d06e585ced5bc1047c3e64051eb21
tukaramgoud <tukaramgoud2804.com>
2024-03-28 08:52:31 +0000

    adding fuctionality JS

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -1,9 +1,17 @@
 // Write your code here
 import {Component} from 'react'
 import './index.css'
+import DenominationItem from '../DenominationItem'
 
 class CashWithdrawal extends Component {
+  state = {amount: 2000}
+
+  checkAmount = rupees => {
+    this.setState(pervState => ({amount: pervState.amount - rupees}))
+  }
+
   render() {
+    const {amount} = this.state
     return (
       <div className="main-container">
         <div className="sub-container">
@@ -12,29 +20,16 @@ class CashWithdrawal extends Component {
             <p className="sarah-name">Sarah Williams</p>
           </div>
           <div className="balance-container">
-            <h1 className="balance-heading">Your Balance</h1>
+            <p className="balance-heading">Your Balance</p>
             <div>
-              <h1 className="rs-in-digits"> 2000</h1>
+              <h1 className="rs-in-digits">{amount}</h1>
               <p className="rs-in-words">In Rupees</p>
             </div>
           </div>
 
-          <h1 className="sarah-name">Withdraw</h1>
+          <p className="sarah-name">Withdraw</p>
           <p className="balance-heading-2">CHOOSE SUM (IN RUPEES)</p>
-          <div>
-            <button type="button" className="button">
-              50
-            </button>
-            <button type="button" className="button">
-              100
-            </button>
-            <button type="button" className="button">
-              200
-            </button>
-            <button type="button" className="button">
-              500
-            </button>
-          </div>
+          <DenominationItem withDraw={this.checkAmount} />
         </div>
       </div>
     )
--- src/components/DenominationItem/index.js
+++ src/components/DenominationItem/index.js
@@ -1,1 +1,41 @@
 // Write your code here
+import './index.css'
+
+const DenominationItem = props => {
+  const {withDraw} = props
+
+  const withDraw50 = () => {
+    withDraw(50)
+  }
+
+  const withDraw100 = () => {
+    withDraw(100)
+  }
+
+  const withDraw200 = () => {
+    withDraw(200)
+  }
+
+  const withDraw500 = () => {
+    withDraw(500)
+  }
+
+  return (
+    <ul className="list-style-make">
+      <li className="button" onClick={withDraw50}>
+        50
+      </li>
+      <li className="button" onClick={withDraw100}>
+        100
+      </li>
+      <li className="button" onClick={withDraw200}>
+        200
+      </li>
+      <li className="button" onClick={withDraw500}>
+        500
+      </li>
+    </ul>
+  )
+}
+
+export default DenominationItem
